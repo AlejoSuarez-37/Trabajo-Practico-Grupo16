34a2821e425aab6f2e9d66136b97ee164afea8b2
LuisP <luisguillermopovis@gmail.com>
2025-10-21 16:07:24 -0300

    se observa si el constructor y la instancia de compacto es correcta

--- tests/compacto.test.ts
+++ tests/compacto.test.ts
@@ -1,10 +1,17 @@
 import Compacto from "../src/vehiculo/compacto"
+import Vehiculo from "../src/vehiculo/vehiculo";
+const compactoTest = new Compacto("AAA111");
+
+
 
 describe("compacto", () => {
-  test("compactotest es unna instancia de comnpacto", () => {
-    const compactoTest = new Compacto("AAA111");
+  test("Se observara si el constructor de compacto crea un compacto es unna instancia de comnpacto", () => {
+
     
     expect(compactoTest).toBeInstanceOf(Compacto);
 
+  });
+  test("Se obvservara si compacto hereda de vehiculo",()=>{
+    expect(compactoTest).toBeInstanceOf(Vehiculo)
   });
 });
