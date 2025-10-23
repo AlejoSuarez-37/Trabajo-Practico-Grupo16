5be09008d0ea2c3602050f56a1b3060c6e0e2641
LuisP <luisguillermopovis@gmail.com>
2025-10-23 14:48:43 -0300

    se examino las tarifas

--- tests/compacto.test.ts
+++ tests/compacto.test.ts
@@ -20,29 +20,51 @@ describe("compacto", () => {
 
     expect(compactoTest.getMatricula()).toBe("AAA111");
 
-  })
+  });
   test("Se observa si el atributo necesitaLimpieza esta en false por defecto",()=>{
 
     expect(compactoTest["necesitaLimpieza"]).toEqual(false);
-  })
+  });
   test("Se obseva si el metodo setNecesitaLimpieza cambia el estado pòr defecto de false a true",()=>{
 
 
     compactoTestDos.setNecesitaLimpieza(true);
 
     expect(compactoTestDos["necesitaLimpieza"]).toEqual(true);
-  })
+  });
   test("Se obseva si el metodo getNecesitaLimpieza devuelve el estado de necesitaLimpieza",()=>{
 
     compactoTestTres["necesitaLimpieza"] = false
 
     expect(compactoTestTres["necesitaLimpieza"]).toEqual(false);
-  })
+  });
   test("comparamos que la tarifa de mantenimiento sea 20 teniendo en cuenta 20 dias para esta prueba",()=>{
     
     let dias:number = 20
     let tarifaEsperada:number = 400
     expect(compactoTest.obtenerTarifaMantenimiento(dias)).toBe(tarifaEsperada);
 
+  });
+  test("comprobamos que la tarifa base con menos de 100km por dia es 30 x dia ",()=>{
+    
+    let dias:number = 30;
+    let kilometrosTotales:number = 250;
+    let tarifaEsperada:number = 900;
+
+    expect(compactoTest.obtenerTarifaReserva(dias,kilometrosTotales)).toBe(tarifaEsperada);
+
+  })
+    test("comprobamos que la tarifa base con mayor o igual de 100km por dia es 30 x dia mas 0.15 xkm",()=>{
+    
+    let dias:number = 30;
+    let kilometrosTotales:number = 3000;
+    let tarifaEsperada:number = 1350;
+
+    expect(compactoTest.obtenerTarifaReserva(dias,kilometrosTotales)).toBe(tarifaEsperada);
+
+    kilometrosTotales = 50000;
+    tarifaEsperada = 8400;
+
+    expect(compactoTest.obtenerTarifaReserva(dias,kilometrosTotales)).toBe(tarifaEsperada);
   })
 });
