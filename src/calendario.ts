fa395f4be154ca221ff10099ed95999ef254026e
Alejo Suarez <alejosuarez731@gmail.com>
2025-11-06 20:04:49 -0300

    Hago el push con los cambios del pull request anterior

--- src/calendario.ts
+++ src/calendario.ts
@@ -2,20 +2,6 @@ import Evento from "./evento";
 import Vehiculo from "./vehiculo/vehiculo";
 
 export default  class Calendario {
-    // static revisarCalendario(fechaInicio: Date, fechaFin: Date, vehiculo: Vehiculo, eventos: Set<Evento>):boolean {
-    //     let value: boolean = true;
-    //     for (const evento of eventos){
-    //         if (evento.getVehiculo() === vehiculo){
-    //             if ((fechaFin > evento.getFechaInicio() && fechaFin < evento.getFechaFin()) || 
-    //                 (fechaInicio < evento.getFechaFin() && fechaInicio > evento.getFechaInicio()) || 
-    //                 (fechaInicio > fechaFin)){
-    //                     value = false;
-    //             }
-    //         }
-    //     }
-    //     return value;
-    // }
-
     static revisarCalendario(evento: Evento, eventos: Set<Evento>):boolean{
         const array = Array.from(eventos);
         const temp: Evento[] = array.filter((eventoTemp: Evento) => eventoTemp.getVehiculo() === evento.getVehiculo());
--- src/cliente.ts
+++ src/cliente.ts
@@ -4,17 +4,6 @@ import Garage from "./garage";
 import Vehiculo from "./vehiculo/vehiculo";
 
 export default class Cliente {
-    // public reservar(fechaInicio: Date, fechaFin: Date, vehiculo: Vehiculo, garage: Garage):void {
-    //     if (!garage.getVehiculos().has(vehiculo.getMatricula())){
-    //         throw new Error("No Existe el vehiculo");
-    //     }
-    //     if (Calendario.revisarCalendario(fechaInicio, fechaFin, vehiculo, garage.getReservas()) && Calendario.revisarCalendario(fechaInicio, fechaFin, vehiculo, garage.getMantenimientos())){
-    //         garage.getReservas().add(new Evento(fechaInicio, fechaFin, vehiculo,garage.getTicket()));
-    //         console.log("Ticket:",garage.getTicket());
-    //         garage.plusTicket();
-    //     }
-    // }
-
     public reservar(evento: Evento, garage: Garage):void {
         if (!garage.getVehiculos().has(evento.getVehiculo().getMatricula())){
             throw new Error("No Existe el vehiculo");
@@ -23,15 +12,6 @@ export default class Cliente {
             garage.getReservas().add(evento);
         }
     }
-
-    // public devolverVehiculo(kilometros: number, ticket: number, garage: Garage):void {
-    //     for (const reserva of garage.getReservas()){
-    //         if (reserva.getTicket() === ticket){
-    //             console.log(reserva.getVehiculo().obtenerTarifaReserva(reserva.getCantDias(),kilometros));
-    //             reserva.getVehiculo().setNecesitaLimpieza(true);
-    //         }
-    //     }
-    // }
 
     public devolverVehiculo(kilometros: number, evento: Evento, garage: Garage):void {
         for (const reserva of garage.getReservas()){
